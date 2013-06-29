90e73138560940410aef689b9b63e9df000a9d34
Skaronator <DayZ@Skaronator.com>
2013-06-29 14:39:32 +0200

    Add "missing" Code from DayZ_Equip
    This Part is currently not needed but if we add more skins then we need
    this part.

--- SQF/dayz_code/Configs/CfgMagazines/Skin.hpp
+++ SQF/dayz_code/Configs/CfgMagazines/Skin.hpp
@@ -1,9 +1,15 @@
 class SkinBase : CA_Magazine {
+		scope = public;
+		count = 1;
+		type = 256;
 		displayName = $STR_EQUIP_NAME_CLOTHES;
 		descriptionShort = $STR_EQUIP_DESC_CLOTHES;
+		model = "\dayz_equip\models\cloth_parcel.p3d";
+		picture = "\dayz_equip\textures\equip_cloth_parcel_ca.paa";
 		class ItemActions {
 			class Use {
 				text = $STR_EQUIP_TEXT_CLOTHES;
+				script = "spawn player_wearClothes;";
 			};
 		};
 	};
