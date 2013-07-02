3d99355e92097bcd3dd62e21b1643ee50a1c41c2
Skaronator <DayZ@Skaronator.com>
2013-07-03 01:00:51 +0200

    Update Version

--- SQF/dayz_code/Configs/rscTitles.hpp
+++ SQF/dayz_code/Configs/rscTitles.hpp
@@ -182,7 +182,7 @@ class RscDisplayMain : RscStandardDisplay
 		class DAYZ_Version : CA_Version
 		{
 			idc = -1;
-			text = "1.0-Devbuild-020713";
+			text = "1.0-Devbuild-030713";
 			y = "(SafeZoneH + SafeZoneY) - (1 - 0.95)";
 		};
 		delete CA_TitleMainMenu;
