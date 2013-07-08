c408a4c9edb3ac534625a99ab812c9f5702f1d62
thevisad <thevisad@gmail.com>
2013-07-08 08:38:25 -0400

    Removed extra item that errors

--- SQF/dayz_code/Configs/CfgMagazines/TrashLoot.hpp
+++ SQF/dayz_code/Configs/CfgMagazines/TrashLoot.hpp
@@ -38,37 +38,7 @@ class ItemTrashToiletpaper : CA_Magazine {
 	};
 
 	
-	class 2Rnd_shotgun_74Slug: 8Rnd_B_Beneli_74Slug
-{
-	displayName="2Rnd. Slug";
-	count=2;
-	descriptionShort="Caliber: 12 gauge <br/>Rounds: 2 <br/>Used in: M1014";
-	model = "\z\addons\dayz_communityassets\models\2shells_slugshot.p3d";
-	picture = "\z\addons\dayz_communityassets\pictures\equip_2shells_slugshot_CA.paa";
-	class ItemActions
-	{
-		class ReloadMag
-		{
-			text="Combine to 8 rounds";
-			script="spawn player_reloadMag;";
-			use[]=
-			{
-				"2Rnd_shotgun_74Slug",
-				"2Rnd_shotgun_74Slug",
-				"2Rnd_shotgun_74Slug",
-				"2Rnd_shotgun_74Slug"
-			};
-			output[]=
-			{
-				"8Rnd_B_Beneli_74Slug"
-			};
-		};
-		class CombineMag {
-			text = $STR_MAG_COMBINE;
-			script = "spawn player_combineMag; r_action_count = r_action_count + 1;";
-		};
-	};
-};
+
 
 	class ItemTrashToiletpaper : ItemBandage{
 		scope = public;
