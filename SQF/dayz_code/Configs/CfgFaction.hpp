a7101a3b84e54757b1ffec089b423968ec1e107f
James Velasquez <xyberviri@gmail.com>
2013-07-17 01:15:24 -0500

    Added Female Sniper suit Added Framework for Factions from Dayz_Factions

---
+++ SQF/dayz_code/Configs/CfgFaction.hpp
@@ -0,0 +1,22 @@
+class CfgFactionClasses
+{
+    class DZU_FACTION_BANDIT
+    {
+        displayName = "Bandit";     //In game faction name that appears in the drop down box under side
+        priority = 1;               //Priority in the faction list in the editor
+        side = 0;                   //In game side, 0=E,1=W,3=R,4=C
+    };
+    class DZU_FACTION_SURVIVOR
+    {
+        displayName = "Survivor";   //In game faction name that appears in the drop down box under side
+        priority = 1;               //Priority in the faction list in the editor
+        side = 1;                   //In game side, 0=E,1=W,3=R,4=C
+    };
+    class DZU_FACTION_ZOMBIE
+    {
+        displayName = "Zombie";     //In game faction name that appears in the drop down box under side
+        priority = 1;               //Priority in the faction list in the editor
+        side = 2;                   //In game side, 0=E,1=W,3=R,4=C
+    };
+
+}:
