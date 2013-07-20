dca9a2ae74f97b8360657e8dd1bdeb4bdcc43388
Skaronator <DayZ@Skaronator.com>
2013-07-20 15:39:41 +0200

    Update Version

--- SQF/dayz_code/config.cpp
+++ SQF/dayz_code/config.cpp
@@ -58,7 +58,7 @@ class CfgMods
 		hidePicture = 0;
 		hideName = 0;
 		action = "http://www.dayzunleashed.com";
-		version = "1.0-Devbuild-070713";
+		version = "1.0-Devbuild-200713";
 		hiveVersion = 0.96;
 	};
 	class DZ_InitWorld
