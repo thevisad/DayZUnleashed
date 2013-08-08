8c54126b4758db25005926444431ab6b8418f1c1
James Velasquez <xyberviri@gmail.com>
2013-08-08 01:59:08 -0500

    Added M1030_DZ & MMT_DZ motorcycle and mountain bike

---
+++ SQF/dayz_code/Configs/CfgVehicles/Bikes/M1030_DZ.hpp
@@ -0,0 +1,9 @@
+class M1030_DZ: M1030_base {
+    displayname = "Motorcycle";
+    faction = "DZU_FACTION_SURVIVOR";
+    crew = "";
+    expansion = 1;
+    scope = 2;
+    side = 1;
+    typicalcargo[] = {};
+};
---
+++ SQF/dayz_code/Configs/CfgVehicles/Bikes/MMT_DZ.hpp
@@ -0,0 +1,8 @@
+class MMT_DZ: MMT_base {
+    displayname = "Unleashed Mountain bike";
+    crew = "";
+    faction = "DZU_FACTION_SURVIVOR";
+    scope = 2;
+    side = 1;
+    typicalcargo[] = {};
+};
