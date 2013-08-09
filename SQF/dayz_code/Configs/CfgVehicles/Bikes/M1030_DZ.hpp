59a65676bdf917c9b0d54e6616297643dd426f9b
James Velasquez <xyberviri@gmail.com>
2013-08-08 23:35:54 -0500

    removed old motorcycles from files that dont need to be derrived.

--- SQF/dayz_code/Configs/CfgVehicles/Bikes/M1030_DZ.hpp
+++
@@ -1,9 +0,0 @@
-class M1030_DZ: M1030_base {
-    displayname = "Motorcycle";
-    faction = "DZU_FACTION_SURVIVOR";
-    crew = "";
-    expansion = 1;
-    scope = 2;
-    side = 1;
-    typicalcargo[] = {};
-};
--- SQF/dayz_code/Configs/CfgVehicles/Bikes/MMT_DZ.hpp
+++
@@ -1,8 +0,0 @@
-class MMT_DZ: MMT_base {
-    displayname = "Unleashed Mountain bike";
-    crew = "";
-    faction = "DZU_FACTION_SURVIVOR";
-    scope = 2;
-    side = 1;
-    typicalcargo[] = {};
-};
