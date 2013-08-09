9d49632e3720fff3b10758c79c14219f13ae142a
James Velasquez <xyberviri@gmail.com>
2013-08-09 17:49:25 -0500

    Revised DZ Classes

---
+++ SQF/dayz_code/Configs/CfgVehicles/Survivors/Revised.hpp
@@ -0,0 +1,26 @@
+class BAF_Soldier_L_MTP_DZ : Camo1_DZ {
+    model = "\ca\characters_d_BAF\BAF_Soldier_light_BAF";
+    hiddenselectionstextures[] = {"\ca\characters_d_baf\data\camo_MTP_co.paa", "\ca\characters_d_baf\data\armour_co.paa", "\ca\characters_d_baf\data\equip_d_co.paa"};
+};
+
+class BAF_Soldier_L_DDPM_DZ : BAF_Soldier_L_MTP_DZ {    
+    hiddenselectionstextures[] = {"\ca\characters_d_baf\data\camo_DDPM_co.paa", "\ca\characters_d_baf\data\armour_co.paa", "\ca\characters_d_baf\data\equip_d_co.paa"};
+};
+
+class BAF_Soldier_DDPM_DZ : Camo1_DZ {
+    hiddenselectionstextures[] = {"\ca\characters_d_baf\data\camo_DDPM_co.paa", "\ca\characters_d_baf\data\armour_co.paa", "\ca\characters_d_baf\data\equip_d_co.paa"};
+};
+
+class BAF_Soldier_MTP : Camo1_DZ {
+    model = "\ca\characters_d_BAF\BAF_Soldier_1_BAF";
+    hiddenselectionstextures[] = {"\ca\characters_d_baf\data\camo_MTP_co.paa", "\ca\characters_d_baf\data\armour_co.paa", "\ca\characters_d_baf\data\equip_d_co.paa"};
+};
+
+class BAF_Soldier_Officer_MTP_DZ : Rocket_DZ {
+    displayName = "Survivor";
+    hiddenselectionstextures[] = {"\ca\characters_d_baf\data\camo_MTP_co.paa", "\ca\characters_d_baf\data\armour_co.paa", "\ca\characters_d_baf\data\equip_d_co.paa"};
+};
+
+class BAF_Soldier_Officer_DDPM_DZ : BAF_Soldier_Officer_MTP_DZ {
+    hiddenselectionstextures[] = {"\ca\characters_d_baf\data\camo_MTP_co.paa", "\ca\characters_d_baf\data\armour_co.paa", "\ca\characters_d_baf\data\equip_d_co.paa"};
+};    
