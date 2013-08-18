83968b3c8cd82dd14228b879105ed1365ecc97dd
[VB]AWOL <vbawol@veteranbastards.com>
2013-08-18 11:15:07 -0500

    buildables to inherit builtitems class DONE

--- SQF/dayz_code/Configs/CfgVehicles/DZE/BuiltItems.hpp
+++ SQF/dayz_code/Configs/CfgVehicles/DZE/BuiltItems.hpp
@@ -1,5 +1,9 @@
 //class NonStrategic;
 class BuiltItems: NonStrategic{};
+class BuiltItemsDestructBase : BuiltItems
+{
+	class DestructionEffects;
+};
 // buildables
 class Hedgehog_DZ: BuiltItems
 {
@@ -213,4 +217,354 @@ class DesertLargeCamoNet_DZ: BuiltItems
 	accuracy = 0.2; 
 	cost = 0; 
 	ladders[] = {}; 
+};
+
+class DeerStand_DZ: BuiltItems
+{
+	scope = 2;
+	offset[] = {0,5,0};
+	displayName = "Deer Stand";
+	vehicleClass = "Fortifications";
+	removeoutput[] = {{"deer_stand_kit",1}};
+	animated = 0; 
+	simulation = "house"; 
+	accuracy = 0.2; 
+	cost = 0; 
+	armor = 500; 
+	model = "\Ca\Structures\Misc\Misc_DeerStand\misc_deerstand"; 
+	ladders[] = {{"start1", "end1"}}; 
+	icon = "\Ca\Structures\Misc\Misc_DeerStand\data\icon\icon_deerstand_ca.paa"; 
+	mapSize = 3; 
+};
+
+class ForestLargeCamoNet_DZ: BuiltItems
+{
+	armor = 300;
+	//armor = 3; 
+	destrType = "DestructNo";
+	//destrType = "DestructTent"; 
+	scope = 2;
+	offset[] = {0,9,0};
+	displayName = "Large Forest Camo Net";
+	vehicleClass = "Fortifications";
+	removeoutput[] = {{"forest_large_net_kit",1}};
+	icon = "\Ca\misc3\data\Icons\icon_camoNetBig_ca.paa"; 
+	model = "\Ca\misc3\CamoNetB_EAST"; 
+	animated = 0; 
+	simulation = "house"; 
+	accuracy = 0.2; 
+	cost = 0; 
+	ladders[] = {}; 
+};
+
+class SandNest_DZ: BuiltItems
+{
+	scope = 2;
+	offset[] = {0,3,1};
+	displayName = "Sandbag Nest";
+	vehicleClass = "Fortifications";
+	removeoutput[] = {{"sandbag_nest_kit",1}};
+	animated = 0; 
+	simulation = "house"; 
+	accuracy = 0.2; 
+	cost = 0; 
+	ladders[] = {}; 
+	icon = "\Ca\misc3\data\Icons\icon_fortNestSmall_ca.paa"; 
+	mapSize = 5; 
+	armor = 500; 
+	irTarget = 0; 
+    model = "\ca\Misc_E\fortified_nest_small_ep1"; 
+	expansion = 1; 
+	GhostPreview = "Land_fortified_nest_smallPreview"; 
+};
+class MetalGate_DZ: BuiltItems
+{
+	armor = 400;
+	scope = 2;
+	offset[] = {0,2.5,1};
+	displayName = "Rusty Gate";
+	vehicleClass = "Fortifications";
+	removeoutput[] = {{"rusty_gate_kit",1}};
+	animated = 0; 
+	simulation = "house"; 
+	accuracy = 0.2; 
+	cost = 0; 
+	ladders[] = {}; 
+	// armor = 50; 
+	model = "\ca\structures\Wall\Wall_Gate_Ind1_L"; 
+	destrType = "DestructTree"; 
+	actionBegin1 = "OpenDoors"; 
+	actionEnd1 = "OpenDoors"; 
+	class AnimationSources { 
+		class DoorL { 
+		  source = "User"; 
+		  animPeriod = 1; 
+		  initPhase = 0; 
+		};
+	};
+	class UserActions { 
+		class OpenDoors { 
+			displayNameDefault = "<img image='\ca\ui\data\ui_action_open_ca.paa' size='4' />"; 
+			displayName = "Open door"; 
+			position = "DoorL"; 
+			radius = 10; 
+			onlyForPlayer = 0; 
+			condition = "this animationPhase "DoorL" < 0.5"; 
+			statement = "this animate ["DoorL", 1]"; 
+		};
+	class CloseDoors : OpenDoors { 
+		displayNameDefault = "<img image='\ca\ui\data\ui_action_close_ca.paa' size='4' />"; 
+		displayName = "Close door"; 
+		condition = "this animationPhase "DoorL" >= 0.5"; 
+		statement = "this animate ["DoorL", 0]"; 
+		};
+	};
+};
+
+class OutHouse_DZ: BuiltItems
+{
+	armor = 200;
+	// armor = 20; 
+	scope = 2;
+	offset[] = {0,2.5,1};
+	displayName = "Outhouse";
+	vehicleClass = "Fortifications";
+	transportMaxMagazines = 4;
+	transportMaxWeapons = 4;
+	transportMaxBackpacks = 4;
+	constructioncount = 2;
+
+	animated = 1; 
+	destrType = "DestructTent"; 
+	actionBegin1 = "OpenDoors"; 
+	actionEnd1 = "OpenDoors"; 
+
+	simulation = "house"; 
+	accuracy = 0.2; 
+	cost = 0; 
+	ladders[] = {}; 
+	class AnimationSources { 
+		class Dvere1 { 
+		  animPeriod = 1; 
+		};
+	};
+	class UserActions { 
+		class OpenDoors { 
+			displayNameDefault = "<img image='\ca\ui\data\ui_action_open_ca.paa' size='4' />"; 
+			displayName = "Open door"; 
+			position = "osa_dvere1"; 
+			radius = 2.5; 
+			onlyForPlayer = 0; 
+			condition = "this animationPhase "Dvere1" < 0.5"; 
+			statement = "this animate ["Dvere1", 1]"; 
+		};
+		class CloseDoors { 
+		  displayNameDefault = "<img image='\ca\ui\data\ui_action_close_ca.paa' size='4' />"; 
+		  displayName = "Close door"; 
+		  position = "osa_dvere1"; 
+		  radius = 2.5; 
+		  onlyForPlayer = 0; 
+		  condition = "this animationPhase "Dvere1" >= 0.5"; 
+		  statement = "this animate ["Dvere1", 0]"; 
+		};
+	};
+	class transportmagazines
+	{
+		class _xx_ItemTrashToiletpaper
+		{
+			magazine = "ItemTrashToiletpaper";
+			count = 1;
+		};
+	};
+};
+
+class StorageShed_DZ: BuiltItemsDestructBase
+{
+	armor = 800;
+	//armor = 30; 
+	model = "\ca\Buildings2\Shed_small\Shed_M01"; 
+	scope = 2;
+	offset[] = {0,2.5,1};
+	displayName = "Storage Shed";
+	vehicleClass = "Fortifications";
+	transportMaxMagazines = 400;
+	transportMaxWeapons = 40;
+	transportMaxBackpacks = 20;
+	constructioncount = 6;
+	animated = 0; 
+	simulation = "house"; 
+	accuracy = 0.2; 
+	cost = 0; 
+	ladders[] = {}; 
+	
+	class DestructionEffects : DestructionEffects { 
+
+		 class Ruin1 { 
+		  simulation = "ruin"; 
+		  type = "\ca\Buildings2\Shed_small\Shed_M01_ruins"; 
+		  position = ""; 
+		  intensity = 1; 
+		  interval = 1; 
+		  lifeTime = 1; 
+		};
+	};
+};
+
+class Fence_corrugated_DZ: BuiltItems
+{
+	armor = 300;
+	scope = 2;
+	offset[] = {0,2.5,1};
+	removeoutput[] = {{"ItemCorrugated",1}};
+	displayName = "Corrugated Fence";
+	vehicleClass = "Fortifications";
+	TextPlural = "Fences"; 
+	TextSingular = "Fence"; 
+	nameSound = "obj_fence"; 
+	animated = 0; 
+	cost = 0; 
+	icon = "\Ca\buildings\Icons\i_fence_CA.paa";
+	accuracy = 0.3; 
+	mapSize = 5.5; 
+	destrType = "DestructTree"; 
+	model = "\ca\structures\wall\Wall_Tin_4"; 
+};
+
+class WoodShack_DZ: BuiltItems
+{
+	scope = 2;
+	offset[] = {0,2.5,1.3};
+	armor = 400;
+	displayName = "Wooden Shack";
+	vehicleClass = "Fortifications";
+	transportMaxMagazines = 100;
+	transportMaxWeapons = 10;
+	transportMaxBackpacks = 5;
+	constructioncount = 5;
+	animated = 0; 
+	simulation = "house"; 
+	accuracy = 0.2; 
+	cost = 0; 
+	ladders[] = {}; 
+	model = "\ca\buildings\kulna"; 
+	destrType = "DestructTent"; 
+	//armor = 50; 
+};
+class Wooden_shed_DZ: BuiltItemsDestructBase
+{
+	scope = 2;
+	model = "\ca\buildings2\Shed_wooden\Shed_wooden"; 
+	destrType = "DestructBuilding"; 
+	offset[] = {0,2.5,1};
+	armor = 400;
+	// armor = 20; 
+	displayName = "Wooden Shed";
+	vehicleClass = "Fortifications";
+	transportMaxMagazines = 200;
+	transportMaxWeapons = 20;
+	transportMaxBackpacks = 10;
+	mapSize = 2; 
+	simulation = "house"; 
+	accuracy = 0.2; 
+	cost = 0; 
+	ladders[] = {}; 
+	actionBegin1 = "OpenDoors1"; 
+	actionEnd1 = "OpenDoors1"; 
+
+	class AnimationSources { 
+		class dvere1 { 
+			animPeriod = 1; 
+			initPhase = 0; 
+		};
+	};
+	class DestructionEffects : DestructionEffects { 
+		class Ruin1 { 
+			simulation = "ruin"; 
+			type = "\ca\buildings2\Shed_wooden\Shed_wooden_ruins"; 
+			position = ""; 
+			intensity = 1; 
+			interval = 1; 
+			lifeTime = 1; 
+		};
+	};
+	class UserActions { 
+		class OpenDoors1 { 
+			displayNameDefault = "<img image='\ca\ui\data\ui_action_open_ca.paa' size='4' />"; 
+			displayName = "Open door"; 
+			position = "osa_dvere1"; 
+			radius = 3; 
+			onlyForPlayer = 0; 
+			condition = "this animationPhase "dvere1" >= 0.5"; 
+			statement = "this animate ["dvere1", 0]"; 
+		};
+		class CloseDoors1 { 
+			displayNameDefault = "<img image='\ca\ui\data\ui_action_close_ca.paa' size='4' />"; 
+			displayName = "Close door"; 
+			position = "osa_dvere1"; 
+			radius = 3; 
+			onlyForPlayer = 0; 
+			condition = "this animationPhase "dvere1" < 0.5"; 
+			statement = "this animate ["dvere1", 1]"; 
+		};
+	};
+};
+class WoodGate_DZ: BuiltItems
+{
+	scope = 2;
+	destrType = "DestructTree"; 
+	//cost = 100;
+	offset[] = {0,1.5,0.5};
+	model = "\ca\structures\Wall\Gate_wood2_5";
+	icon = "\ca\data\data\Unknown_object.paa";
+	mapSize = 2;
+	armor = 100;
+	displayName = "Wood Panel";
+	vehicleClass = "Fortifications";
+	
+	class AnimationSources
+	{
+		class DoorR { 
+		  source = "User"; 
+		  animPeriod = 1; 
+		  initPhase = 0; 
+		};
+	};
+	class UserActions
+	{
+		class CloseDoor
+		{
+			position = "";
+			displayName = "Close Door";
+			radius = 1.5;
+			onlyForPlayer = 0;
+			condition = "this animationPhase 'DoorR' == 1";
+			statement = "this animate ['DoorR', 0];";
+		};
+		class OpenDoor
+		{
+			position = "";
+			displayName = "Open Door";
+			radius = 1.5;
+			onlyForPlayer = 0;
+			condition = "this animationPhase 'DoorR' == 0";
+			statement = "this animate ['DoorR', 1];";
+		};
+	};
+};
+class StickFence_DZ: BuiltItems
+{
+	destrType = "DestructTree"; 
+	armor = 200;
+	scope = 2;
+	offset[] = {0,2.5,0};
+	displayName = "Stick Fence";
+	vehicleClass = "Fortifications";
+	animated = 0; 
+	simulation = "house"; 
+	accuracy = 0.2; 
+	cost = 0; 
+	ladders[] = {}; 
+	expansion = 1; 
+	model = "\ca\Structures_E\Wall\Wall_L\Wall_FenW2_6_EP1"; 
+	//armor = 300; 
 };
--- SQF/dayz_code/Configs/CfgVehicles/DZE/MoreBuiltItems.hpp
+++ SQF/dayz_code/Configs/CfgVehicles/DZE/MoreBuiltItems.hpp
@@ -1,7 +1,7 @@
 // TODO: Move to BuiltItems 
 // class Plastic_Pole_EP1;
-class Sign_1L_Noentry_EP1;
-class Plastic_Pole_EP1_DZ: Sign_1L_Noentry_EP1
+class Sign_1L_Noentry;
+class Plastic_Pole_EP1_DZ: Sign_1L_Noentry
 {
 	scope = 2;
 	// destrType = "DestructNo"; 
@@ -9,135 +9,10 @@ class Plastic_Pole_EP1_DZ: Sign_1L_Noentry_EP1
 	displayName = "30m Plot Pole";
 	vehicleClass = "Fortifications";
 	removeoutput[] = {{"30m_plot_kit",1}};
+	expansion = 1; 
+	hiddenSelectionsTextures[] = {"\ca\signs_e\signm\data\sign_one_leg_h_no_entry_co.paa"}; 
 };
 
-class Land_Misc_deerstand;
-class DeerStand_DZ: Land_Misc_deerstand
-{
-	armor = 300;
-	// destrType = "DestructNo";
-	scope = 2;
-	offset[] = {0,5,0};
-	displayName = "Deer Stand";
-	vehicleClass = "Fortifications";
-	removeoutput[] = {{"deer_stand_kit",1}};
-};
-class Land_CamoNetB_EAST;
-class ForestLargeCamoNet_DZ: Land_CamoNetB_EAST
-{
-	armor = 300;
-	destrType = "DestructNo";
-	scope = 2;
-	offset[] = {0,9,0};
-	displayName = "Large Forest Camo Net";
-	vehicleClass = "Fortifications";
-	removeoutput[] = {{"forest_large_net_kit",1}};
-};
-
-class Land_fortified_nest_small_EP1;
-class SandNest_DZ: Land_fortified_nest_small_EP1
-{
-	armor = 400;
-	scope = 2;
-	offset[] = {0,3,1};
-	displayName = "Sandbag Nest";
-	vehicleClass = "Fortifications";
-	removeoutput[] = {{"sandbag_nest_kit",1}};
-};
-class Land_Wall_Gate_Ind1_L;
-class MetalGate_DZ: Land_Wall_Gate_Ind1_L
-{
-	armor = 400;
-	scope = 2;
-	offset[] = {0,2.5,1};
-	displayName = "Rusty Gate";
-	vehicleClass = "Fortifications";
-	removeoutput[] = {{"rusty_gate_kit",1}};
-};
-class Land_KBud;
-class OutHouse_DZ: Land_KBud
-{
-	armor = 200;
-	scope = 2;
-	offset[] = {0,2.5,1};
-	displayName = "Outhouse";
-	vehicleClass = "Fortifications";
-	transportMaxMagazines = 4;
-	transportMaxWeapons = 4;
-	transportMaxBackpacks = 4;
-	constructioncount = 2;
-	class transportmagazines
-	{
-		class _xx_ItemTrashToiletpaper
-		{
-			magazine = "ItemTrashToiletpaper";
-			count = 1;
-		};
-	};
-};
-class Land_Shed_M01;
-class StorageShed_DZ: Land_Shed_M01
-{
-	armor = 800;
-	scope = 2;
-	offset[] = {0,2.5,1};
-	displayName = "Storage Shed";
-	vehicleClass = "Fortifications";
-	transportMaxMagazines = 400;
-	transportMaxWeapons = 40;
-	transportMaxBackpacks = 20;
-	constructioncount = 6;
-};
-class Fence_corrugated_plate;
-class Fence_corrugated_DZ: Fence_corrugated_plate
-{
-	armor = 300;
-	scope = 2;
-	offset[] = {0,2.5,1};
-	removeoutput[] = {{"ItemCorrugated",1}};
-	displayName = "Corrugated Fence";
-	vehicleClass = "Fortifications";
-	
-};
-class Land_kulna;
-class WoodShack_DZ: Land_kulna
-{
-	scope = 2;
-	destrType = "DestructBuilding"; 
-	offset[] = {0,2.5,1.3};
-	armor = 400;
-	displayName = "Wooden Shack";
-	vehicleClass = "Fortifications";
-	transportMaxMagazines = 100;
-	transportMaxWeapons = 10;
-	transportMaxBackpacks = 5;
-	constructioncount = 5;
-};
-class Land_Shed_wooden;
-class Wooden_shed_DZ: Land_Shed_wooden
-{
-	scope = 2;
-	destrType = "DestructBuilding"; 
-	//cost = 100;
-	offset[] = {0,2.5,1};
-	armor = 400;
-	displayName = "Wooden Shed";
-	vehicleClass = "Fortifications";
-	transportMaxMagazines = 200;
-	transportMaxWeapons = 20;
-	transportMaxBackpacks = 10;
-};
-
-class Wall_FenW2_6_EP1;
-class StickFence_DZ: Wall_FenW2_6_EP1
-{
-	destrType = "DestructTree"; 
-	armor = 200;
-	scope = 2;
-	offset[] = {0,2.5,0};
-	displayName = "Stick Fence";
-	vehicleClass = "Fortifications";
-};
 /* Needs ASC EU Lights Mod http://www.armaholic.com/page.php?id=12076
 class ASC_EU_LHVOld;
 class LightPole_DZ: ASC_EU_LHVOld
@@ -149,46 +24,3 @@ class LightPole_DZ: ASC_EU_LHVOld
 	vehicleClass = "Fortifications";
 };
 */
-class WoodGate_DZ: BuiltItems
-{
-	scope = 2;
-	destrType = "DestructTree"; 
-	//cost = 100;
-	offset[] = {0,1.5,0.5};
-	model = "\ca\structures\Wall\Gate_wood2_5";
-	icon = "\ca\data\data\Unknown_object.paa";
-	mapSize = 2;
-	armor = 100;
-	displayName = "Wood Panel";
-	vehicleClass = "Fortifications";
-	
-	class AnimationSources
-	{
-		class DoorR { 
-		  source = "User"; 
-		  animPeriod = 1; 
-		  initPhase = 0; 
-		};
-	};
-	class UserActions
-	{
-		class CloseDoor
-		{
-			position = "";
-			displayName = "Close Door";
-			radius = 1.5;
-			onlyForPlayer = 0;
-			condition = "this animationPhase 'DoorR' == 1";
-			statement = "this animate ['DoorR', 0];";
-		};
-		class OpenDoor
-		{
-			position = "";
-			displayName = "Open Door";
-			radius = 1.5;
-			onlyForPlayer = 0;
-			condition = "this animationPhase 'DoorR' == 0";
-			statement = "this animate ['DoorR', 1];";
-		};
-	};
-};
