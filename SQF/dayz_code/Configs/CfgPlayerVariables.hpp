a805b5d8664112138743ea57dcee14c1874fbbbc
James Velasquez <xyberviri@gmail.com>
2013-10-30 21:06:26 -0500

    Added save code for server side of player variable sysetem

--- SQF/dayz_code/Configs/CfgPlayerVariables.hpp
+++ SQF/dayz_code/Configs/CfgPlayerVariables.hpp
@@ -15,43 +15,39 @@ VaribleName        _var = getText(_config >> _VAR >> "pVar");
 */
 
 class CfgPlayerVariables{
-    class default {  
-      id=0;         //number:
+    class default {
       desc="default variable skill";      //  text: Friendly description 
-      load=0;       //  bool: should DZU_fnc_loadVaribles load this?
-      hide=0;       //  bool: listing varibles in a UI should this be shown?
-      global=0;     //  bool: _obj setvarible[_somthing,_something,_this];
-      vardefault=1;    //   var: _obj setvarible[_somthing,_this,_something];  
-      varname="";   //  text: _obj setvarible[_this,_somthing,_something]; 
+      load=0;           //  bool: should DZU_fnc_loadVaribles load this?
+      hide=0;           //  bool: listing varibles in a UI should this be shown?
+      global=0;         //  bool: _obj setvarible[_somthing,_something,_this];
+      vardefault=1;     //   var: _obj setvarible[_somthing,_this,_something];  
+      varname="";       //  text: _obj setvarible[_this,_somthing,_something]; 
       vartype="none";   //  text: "array","number",text,"object","raw","none"
-      varmin=1;     //  read: depends on type: array affects size, number affects limit, text affects length, object is ignored, raw is ignored.    
-      varmax=100;      //  read: depends on type: array affects size, number affects limit, text affects length, object is ignored, raw is ignored. 
-      limit=0;      //  read: depends on type: array affects number of items, number affects max change, text affects length, object is ignored, raw is ignored. 
+      varmin=1;         //  read: depends on type: array affects size, number affects limit, text affects length, object is ignored, raw is ignored.    
+      varmax=100;       //  read: depends on type: array affects size, number affects limit, text affects length, object is ignored, raw is ignored. 
+      limit=0;          //  read: depends on type: array affects number of items, number affects max change, text affects length, object is ignored, raw is ignored.
+      forcedSave=0;     //  force this variable to save every time it changes?
     };
     class Engineer : default {
          load=1;
-           id=1;
          desc="Engineering";
       varname="vEN"; 
       vartype="number";
     };    
     class Hunting : default {
-         load=1;        
-           id=2;
+         load=1;
          desc="Hunting";
       varname="vHN"; 
       vartype="number";
     };
     class Medical : default {
          load=1;
-           id=3;
          desc="Medical Knowledge";
       varname="vMK";  
       vartype="number"; 
     };
     class Combat : default {
          load=1;
-           id=4;
          desc="Combat Training";
       varname="vCT"; 
       vartype="number";
@@ -85,8 +81,17 @@ class CfgPlayerVariables{
          vardefault=0;
              varmin=0;
              varmax=9999;
-        desc="Pin";
+           desc="Pin";
         varname="vPN"; 
+     vartype="number";
+    };
+    class OneTimePass : default {
+               load=1;
+         vardefault=0;
+             varmin=0;
+             varmax=9999;
+        desc="Pass";
+        varname="vPA"; 
      vartype="number";
     };
     class Reg : default {
