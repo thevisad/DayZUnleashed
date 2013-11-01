08982b7982a2b4d7a0c4c552632f5f99c8c2781e
James Velasquez <xyberviri@gmail.com>
2013-10-31 21:46:23 -0500

    reverted some of the variable save code to prerace hotfix initial meat rewrite

--- SQF/dayz_code/Configs/CfgMagazines/FoodMeatRaw.hpp
+++ SQF/dayz_code/Configs/CfgMagazines/FoodMeatRaw.hpp
@@ -4,6 +4,8 @@ class FoodmeatRaw : FoodRaw {
 	picture = "\dayz_equip\textures\equip_steak_ca.paa";
 	descriptionShort = $STR_FOOD_EQUIP_CODE_DESC_0;
 	bloodRegen = 100;
+    cooked ="FoodmeatCooked";
+    badmeat="Foodmeatbad";
 };
 class FoodmuttonRaw : FoodRaw {
 	displayName = $STR_FOOD_EQUIP_CODE_NAME_1;
@@ -11,6 +13,8 @@ class FoodmuttonRaw : FoodRaw {
 	model = "z\addons\dayz_communityassets\models\mutton_raw.p3d";
 	picture = "\z\addons\dayz_communityassets\pictures\equip_mutton_raw_CA.paa";
 	bloodRegen = 100;
+    cooked ="FoodmuttonCooked";
+    badmeat="Foodmuttonbad";
 };
 
 class FoodchickenRaw : FoodRaw {
@@ -19,6 +23,8 @@ class FoodchickenRaw : FoodRaw {
 	model = "\dayz_equip\models\food_steak_gear.p3d";
 	picture = "\dayz_equip\textures\equip_steak_ca.paa";	
 	bloodRegen = 100;
+    cooked ="FoodchickenCooked";
+    badmeat="Foodchickenbad";
 };
 
 class FoodrabbitRaw : FoodRaw {
@@ -27,6 +33,8 @@ class FoodrabbitRaw : FoodRaw {
 	model = "\dayz_equip\models\food_steak_gear.p3d";
 	picture = "\dayz_equip\textures\equip_steak_ca.paa";	
 	bloodRegen = 200;
+    cooked ="FoodrabbitCooked";
+    badmeat="Foodrabbitbad";
 };
 
 class FoodbaconRaw : FoodRaw {
@@ -35,6 +43,8 @@ class FoodbaconRaw : FoodRaw {
 	picture = "\z\addons\dayz_communityassets\pictures\equip_bacon_raw_CA.paa";
 	descriptionShort = $STR_FOOD_EQUIP_CODE_DESC_4;
 	bloodRegen = 150;
+    cooked ="FoodbaconCooked";
+    badmeat="Foodbaconbad";
 };
 class FoodbeefRaw : FoodRaw {
 	displayName = $STR_FOOD_EQUIP_CODE_NAME_6;
@@ -42,6 +52,8 @@ class FoodbeefRaw : FoodRaw {
 	model = "\dayz_equip\models\food_steak_gear.p3d";
 	picture = "\dayz_equip\textures\equip_steak_ca.paa";	
 	bloodRegen = 100;
+    cooked ="FoodbeefCooked";
+    badmeat="Foodbeefbad";
 };
 class FoodgoatRaw : FoodRaw {
 	displayName = $STR_FOOD_NAME_GOAT_RAW;
@@ -49,4 +61,6 @@ class FoodgoatRaw : FoodRaw {
 	model = "\dayz_equip\models\food_steak_gear.p3d";
 	picture = "\dayz_equip\textures\equip_steak_ca.paa";	
 	bloodRegen = 100;
+    cooked ="FoodbeefCooked";
+    badmeat="Foodbeefbad";
 };
